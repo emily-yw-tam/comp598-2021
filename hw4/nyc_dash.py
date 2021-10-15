a724600eebf1297b300ec375383261fc7cfcfca1
emily-yw-tam <77943163+emily-yw-tam@users.noreply.github.com>
2021-10-15 19:48:02 -0400

    Add files via upload

--- hw4/nyc_dash.py
+++ hw4/nyc_dash.py
@@ -5,6 +5,7 @@ http://3.15.12.55:8080/nyc_dash?username=nyc&password=iheartnyc
 from bokeh.layouts import column, row
 from bokeh.plotting import curdoc, figure
 from bokeh.models import ColumnDataSource, Dropdown, Legend, LegendItem
+from bokeh.models.tickers import FixedTicker
 import json
 
 # authenticate any user who logs in with URL parameters username=nyc and password=iheartnyc
@@ -32,20 +33,26 @@ def load_data():
     return all_data, zipcode_data, zipcodes
 
 def update_dropdown1(event):
-    og = source.data['y_values'][2]
-    source.data['y_values'] = [all_data, zipcode_data[event.item], og]
+    og1 = source.data['y_values'][2]
+    source.data['y_values'] = [all_data, zipcode_data[event.item], og1]
+    
+    og2 = source.data['legend'][2]
+    z = f'Zipcode 1: {event.item}'
+    source.data['legend'] = ['All 2020 data', z, og2]
 
     p.y_range.start = 0
-    p.y_range.end = max(max(all_data), max(zipcode_data[event.item]), max(og))
-
-    print(f"HI {event.item} {type(event.item)} cri")
+    p.y_range.end = (max(max(all_data), max(zipcode_data[event.item]), max(og1)))+10
 
 def update_dropdown2(event):
-    og = source.data['y_values'][1]
-    source.data['y_values'] = [all_data, og, zipcode_data[event.item]]
+    og1 = source.data['y_values'][1]
+    source.data['y_values'] = [all_data, og1, zipcode_data[event.item]]
+
+    og2 = source.data['legend'][1]
+    z = f'Zipcode 2: {event.item}'
+    source.data['legend'] = ['All 2020 data', og2, z]
 
     p.y_range.start = 0
-    p.y_range.end = max(max(all_data), max(og), max(zipcode_data[event.item]))
+    p.y_range.end = (max(max(all_data), max(og1), max(zipcode_data[event.item])))+10
 
 # authenticate user
 c = curdoc()
@@ -64,14 +71,18 @@ red = 'red'
 green = 'limegreen'
 blue = 'dodgerblue'
 
+# customize hover tool
+h_tooltips = [("(x,y)", "($x, $y)")]
+
 # create plot
-p = figure(x_range=(0,12), y_range=(0,170), x_axis_label='Month', y_axis_label='Average Response Time (Hours)', toolbar_location=None, title='Difference in response time to complaints filed through the 311 service by zipcode')
+p = figure(x_range=(0,12), y_range=(0,170), x_axis_label='Month', y_axis_label='Average Response Time (Hours)', toolbar_location='right', tools='pan,box_zoom,zoom_in,zoom_out,hover,save,reset', tooltips=h_tooltips, title='Difference in response time to complaints filed through the 311 service by zipcode')
 source = ColumnDataSource({'x_values':[months, months, months], 'y_values':[all_data, zeroes, zeroes], 'colours':[red, green, blue], 'legend':['All 2020 data', 'Zipcode 1', 'Zipcode 2']})
 
 # line plot of monthly average response time for ALL 2020 data
-p.multi_line(xs='x_values', ys='y_values', line_color='colours', line_width=2, legend_group='legend', source=source)
+p.multi_line(xs='x_values', ys='y_values', line_color='colours', line_width=2, legend='legend', source=source)
 
-print(f'hello {type(zipcodes)}')
+# tick locations
+p.xaxis.ticker = FixedTicker(ticks=[0,1,2,3,4,5,6,7,8,9,10,11,12])
 
 # dropdown for selecting zipcode 1
 d1 = Dropdown(label='Zipcode 1', background=green, button_type='success', menu=zipcodes)
@@ -83,4 +94,6 @@ d2.on_click(update_dropdown2)
 
 # put the button and plot in a layout and add to the document
 c.add_root(column(p, d1, d2))
+
+
 
